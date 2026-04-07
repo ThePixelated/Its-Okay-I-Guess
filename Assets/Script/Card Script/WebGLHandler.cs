417cdb055baffba5db9a0c9504f3a56fbc0e63d4
Jack <44131061+ThePixelated@users.noreply.github.com>
2026-04-07 09:15:16 +0700

    transtition to laptop - puish latest

--- Assets/Script/Card Script/WebGLHandler.cs
+++ Assets/Script/Card Script/WebGLHandler.cs
@@ -1,21 +1,24 @@
 using UnityEngine;
 using System.Runtime.InteropServices;
+
 public class WebGLHandler : MonoBehaviour
 {
-    [DllImport("__Internal")]
-    public static extern bool IsMobileBrowser();
+    #if UNITY_WEBGL && !UNITY_EDITOR
+        [DllImport("__Internal")]
+        public static extern bool IsMobileBrowser();
+    #endif
 }
 
 public class Platform
 {
     public static bool IsMobileBrowser()
     {
-#if UNITY_EDITOR
-        return false; // value to return in Play Mode (in the editor)
-#elif UNITY_WEBGL
-    return WebGLHandler.IsMobileBrowser(); // value based on the current browser
-#else
-    return false; // value for builds other than WebGL
-#endif
+        #if UNITY_WEBGL && !UNITY_EDITOR
+                // Panggil fungsi JS cuma kalo di build WebGL asli
+                return WebGLHandler.IsMobileBrowser(); 
+        #else
+                // Editor, Windows, Mac, dll balikannya false
+                return false;
+        #endif
     }
 }
